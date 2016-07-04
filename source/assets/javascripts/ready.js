6e311f1d13948473f8b35a30fdc79ad26e1bc42b
Ivan Gonzalez <ivan@simplelogica.net>
2016-07-04 23:52:18 +0200

    Add some Javascript functions to get info through the Spotify’s API

--- source/assets/javascripts/ready.js
+++ source/assets/javascripts/ready.js
@@ -1,3 +1,18 @@
 $(function() {
   SocialShareKit.init();
+
+  function get_cover_art(artist_id, album_index) {
+    $.get("https://api.spotify.com/v1/artists/" + artist_id + "/albums", function(data) {
+      $(".album-cover").attr("src", data['items'][album_index]['images'][1]['url']);
+      console.log(data['items'][0]);
+    }).fail(function() {
+      $(".album-cover").attr("src", "");
+    });
+  }
+
+  if($(".album").length > 0) {
+    $.each($(".album"), function(element) {
+      get_cover_art($(this).attr("data-spotify-id"), $(this).attr("data-album-index"));
+    });
+  }
 });
