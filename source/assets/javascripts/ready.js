332f1184243626028a718fecf20c1ef0c20adf7d
Ivan Gonzalez <ivan@simplelogica.net>
2016-07-06 11:34:43 +0200

    Complete JS code to get the artist and album info from Spotify API

--- source/assets/javascripts/ready.js
+++ source/assets/javascripts/ready.js
@@ -1,18 +1,41 @@
 $(function() {
   SocialShareKit.init();
 
-  function get_cover_art(artist_id, album_index) {
+  function get_album_info(artist_id, album_index) {
     $.get("https://api.spotify.com/v1/artists/" + artist_id + "/albums", function(data) {
       $(".album-cover").attr("src", data['items'][album_index]['images'][1]['url']);
-      console.log(data['items'][0]);
+      $(".playlist").attr("src", "https://embed.spotify.com/?uri=" + data['items'][album_index]['uri'])
+                    .width($(".main").width());
     }).fail(function() {
       $(".album-cover").attr("src", "");
+      $(".playlist").attr("src", "");
     });
   }
 
-  if($(".album").length > 0) {
-    $.each($(".album"), function(element) {
-      get_cover_art($(this).attr("data-spotify-id"), $(this).attr("data-album-index"));
+  function get_artist_info(artist_id) {
+    $.get("https://api.spotify.com/v1/artists/" + artist_id, function(data) {
+      $(".artist-image").attr("src", data['images'][0]['url']);
+    }).fail(function() {
+      $(".artist-image").attr("src", "");
     });
+  }
+
+  // Init album details from Spotify API
+  if($(".album-detail").length > 0) {
+    $.each($(".album-detail"), function(element) {
+      get_album_info($(this).attr("data-spotify-id"), $(this).attr("data-album-index"));
+    });
+  }
+
+  // Init artist details from Spotify API
+  if($(".artist-detail").length > 0) {
+    $.each($(".artist-detail"), function(element) {
+      get_artist_info($(this).attr("data-spotify-id"));
+    });
+  }
+
+  // Modify the width of the about playlist
+  if($("#about .playlist").length > 0) {
+    $("#about .playlist").width($("#about .main").width());
   }
 });
